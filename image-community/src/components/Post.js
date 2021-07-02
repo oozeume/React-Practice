b19b3c6798f2440633b8805de69590eddff3412c
Woo Jieum <oozeume@gmail.com>
2021-07-02 15:30:34 +0900

    심화 프로젝트 파일 생성

---
+++ image-community/src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Post = (props) => {
+    return(
+        <React.Fragment>
+            {/* 이런게 필요하다 미리 작성해두고 시작 (이 정보들을 props로 받아올거라는 얘기) */}
+            {/* user profile / user name / insert_dt / is_me btn (edit btn) */}
+            {/* contents */}
+            {/* image*/}
+            {/* comment count*/}
+
+            <div>user profile / user name / insert_dt / is_me btn (edit btn)</div>
+            <div>contents</div>
+            <div>image</div>
+            <div>comment count</div>
+
+        </React.Fragment>
+    )
+}
+
+export default Post;
